b2f63e15ed8575e6c67ff58b947f24b48d7af322
jobayermojumder <jobayer.pro@gmail.com>
2017-04-12 22:10:13 +0600

    all

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+   /*if(empty($_SESSION['email'])){
+       header("Location:login.php");
+   }*/
+
+    /*if(isset($_GET['page'])){
+        switch($_GET["page"]){
+            case "dashboard":
+                include 'dashboard.php';
+                break;
+            case "add_profile":
+                include 'dashboard.php';
+                break;
+        }
+    }*/
+    /*else
+*/        include 'login.php';
+
+?>
